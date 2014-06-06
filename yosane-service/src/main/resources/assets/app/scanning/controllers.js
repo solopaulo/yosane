43cd17f505b269ed0f6f61fcc7e33824fc79d881
paul symons <official@iove.org>
2014-06-07 00:08:36 +0800

    building a bootstrap/angular layout. selection of scanner

--- yosane-service/src/main/resources/assets/app/scanning/controllers.js
+++ yosane-service/src/main/resources/assets/app/scanning/controllers.js
@@ -3,7 +3,15 @@
  */
 var yosaneApp = angular.module('yosaneApp');
 yosaneApp.controller('ScanningController',['$scope','yosaneServices',function($scope,yosaneServices) {
-    $scope.scanners = yosaneServices.get({},function(data) {
+    $scope.selected = "";
+    $scope.refreshScannerList = function() {
+        yosaneServices.get({},function(data) {
             $scope.items = data._links.scanner;
-    });
+        }); 
+    };
+    $scope.selectScanner = function(name) {
+      $scope.selected = name;
+    };
+    $scope.refreshScannerList();
+    
 }]);
